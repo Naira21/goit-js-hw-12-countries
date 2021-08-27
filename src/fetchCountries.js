b3a34c122e54d19513ab110f4a246147f7692af2
Naira Kazeian <nkazeian@gmail.com>
2021-08-27 23:13:04 +0300

    updated parcel

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,34 +1,4 @@
-import debounce from 'lodash.debounce';
-debounce(test, 500)
-
-function test() {
-  console.log('object');
-}
-
-import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
-import '@pnotify/core/dist/BrightTheme.css';
-
-  alert({
-    text: 'Too many matches found. Please enter a more specific query!'
-  });
-
-
-
-
-function createCountryCard(arr) {
-  for (let obj of arr) {
-    createFlag(obj)
-  }
-
-}
-
-
-function createFlag(obj) {
-  const svg = document.createElement('svg');
-  const use = svg.appendChild('use');
-  use.src = obj.flag;
-  container.appendChild(svg);
-}
+import './sass/main.scss';
 
 const container = document.querySelector('#root'); //вешаем разметку
 const countrySearch = document.querySelector('#input');
@@ -41,19 +11,24 @@ const handlerSearch = (e) => {
   
   fetch(`https://restcountries.eu/rest/v2/name/${name}`) //после name/ должен быть ${name} которое мы вставили в инпуте
   .then(response =>response.json())  
-  .then(data => console.log(data)) //не выдает массив элементов, поэтому не знаю какой ключ использовать
+  .then(countries => renderCollection(countries.name)) //не выдает массив элементов, поэтому не знаю какой ключ использовать --- здесь должно бытьь имя объекта!!!
   .catch(err => console.log('error', err));
 }
 
+function createCountryCard({flag, name, population, languages}) {
+  const card = `<article>
+    <svg>
+    <use scr='${flag}' alt='Flag of ${name}'></use>
+    </svg> 
+    <p>${name}</p>
+    <p>${population}</p>
+    <p>${languages}</p>
+  </article>`
+  container.insertAdjacentHTML('beforeend', card);
+}
 
+function renderCollection(arr) {
+  arr.forEach(el => createCountryCard(el));
+}
 
-countrySearch.addEventListener('input', handlerSearch)
-
-// function createCountryName({name, population, languages, picture}) {
-//   const name = createElement('p');
-//   p.textContent = `Country: ${name.name}
-//   Population: ${population}
-//   Languages: ${languages}`;
-// }
-
-// {name, flag, population, languages}
+countrySearch.addEventListener('input', handlerSearch);
